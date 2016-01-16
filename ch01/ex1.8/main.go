6bd8801e4bf47555bba3f5d5bfa2e8fe53bb6ec7
Yosuke Kumakura <kumatch@gmail.com>
2016-01-17 00:45:48 +0900

    added exercise for fetching a URL.

---
+++ ch01/ex1.8/main.go
@@ -0,0 +1,39 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Original: https://github.com/adonovan/gopl.io/
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(normalizeURL(url))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+
+func normalizeURL(url string) string {
+	if strings.HasPrefix(url, "http://") {
+		return url
+	}
+
+	return "http://" + url
+}
