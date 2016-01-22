372e81ab7581a5d25bcb90b35d297ce12d535734
Yosuke Kumakura <kumatch@gmail.com>
2016-01-23 01:19:33 +0900

    added exercise for fetching concurrently.

---
+++ ch01/ex1.10-11/ex11.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+go run main.go \
+  https://www.google.com/ \
+  https://facebook.com/ \
+  http://youtube.com/ \
+  http://baidu.com/ \
+  http://yahoo.com/ \
+  http://www.amazon.com/ \
+  http://wikipedia.org/ \
+  http://qq.com/ \
+  http://Google.co.in/ \
+  http://twitter.com/
---
+++ ch01/ex1.10-11/main.go
@@ -0,0 +1,57 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+// Original: https://github.com/adonovan/gopl.io/
+
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	file, err := os.Create("fetchall_output.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		_, err := file.WriteString(<-ch + "\n")
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
+
+//!-
